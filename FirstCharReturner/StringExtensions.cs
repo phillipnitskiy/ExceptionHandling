225a2ecdb168b8395325def0727e63cbef05049c
Pilip Lipnitski <phillipnitskiy@gmail.com>
2017-10-22 14:06:40 +0300

    Converting to int completed. XML-comments added. Exceptions updated.

---
+++ FirstCharReturner/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FirstCharReturner
+{
+    /// <summary>
+    /// Extension methods for strings.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Finds first char in a string.
+        /// </summary>
+        /// <param name="s">A string to take first char from.</param>
+        /// <returns>First char contained in s.</returns>
+        public static char FirstChar(this string s)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException(nameof(s));
+            }
+            if (s.Length == 0)
+            {
+                throw new FormatException("Input string cannot be empty.");
+            }
+
+            return s[0];
+        }
+    }
+}
